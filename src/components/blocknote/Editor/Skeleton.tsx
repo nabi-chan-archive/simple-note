a3b1b95c7921dd5e625d714942a4bbfd4cb15a93
nabi.lee <hello@nabi.kim>
2023-07-18 15:41:02 +0900

    Add EditorSkeleton

---
+++ src/components/blocknote/Editor/Skeleton.tsx
@@ -0,0 +1,15 @@
+export default function EditorSkeleton() {
+  return (
+    <div className="flex flex-col gap-2">
+      {[...Array<void>(10)].map((_, i) => (
+        <div key={i}>
+          <span className="skeleton text-transparent">
+            {[...Array<void>(12)].map((_, j) => (
+              <span key={j} className="inline-block h-4 w-20" />
+            ))}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+}
