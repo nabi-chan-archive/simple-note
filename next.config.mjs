4ad62f032de3b997a8d5f8d59ced92d0c0701947
nabi.lee <hello@nabi.kim>
2023-07-19 12:50:36 +0900

    format next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,13 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-
   transpilePackages: ["jotai-devtools"],
 };
 
